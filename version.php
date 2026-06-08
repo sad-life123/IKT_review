f2ad3f56e429cd9b6b32994d19c8b653717c44ab
anneromqqq <ann.sulzhyts@gmail.com>
2026-06-08 16:30:11 +0300

    style(chart): add padding for percentages and optimize text position for small values

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ikt_review';
-$plugin->version = 2028052511;
+$plugin->version = 2028052513;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.3.1';
